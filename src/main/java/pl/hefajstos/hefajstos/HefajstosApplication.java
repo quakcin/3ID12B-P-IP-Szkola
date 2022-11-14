8d29192a4d9cd79ef64382b21d0c7956d4043253
bandurama <banduram1@gmail.com>
2022-11-14 21:36:52 +0100

    Początek

---
+++ src/main/java/pl/hefajstos/hefajstos/HefajstosApplication.java
@@ -0,0 +1,13 @@
+package pl.hefajstos.hefajstos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HefajstosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HefajstosApplication.class, args);
+    }
+
+}
