2cfa2990c568196cb999da9fd370d829e49f8d4a
bandurama <banduram1@gmail.com>
2022-11-16 22:06:49 +0100

    Początek3

--- src/main/java/pl/hefajstos/hefajstos/HefajstosApplication.java
+++ src/main/java/pl/hefajstos/hefajstos/HefajstosApplication.java
@@ -1,13 +1,35 @@
 package pl.hefajstos.hefajstos;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
 
 @SpringBootApplication
-public class HefajstosApplication
-{
+public class HefajstosApplication implements CommandLineRunner {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private Studenci stud;
+
     public static void main(String[] args) {
         SpringApplication.run(HefajstosApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        /*String sql = "SELECT * FROM Uczen";
+
+        List<Student> students = jdbcTemplate.query(sql,
+                BeanPropertyRowMapper.newInstance(Student.class));*/
+
+        stud.getUczniowie().forEach(System.out :: println);
     }
 
 }
