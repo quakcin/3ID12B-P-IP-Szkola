cdb742b9031a104e16b8b02e7b5ba7602bcee838
bandurama <banduram1@gmail.com>
2023-01-13 01:18:23 +0100

    Aktualizacja plików dotyczących bazy danych.

---
+++ Oracle SQL/przedmioty.sql
@@ -0,0 +1,70 @@
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Język polski', 4, 5, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Język polski', 5, 4, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Język polski', 6, 5, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Język polski', 7, 5, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Język polski', 8, 5, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Język angielski', 4, 3, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Język angielski', 5, 3, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Język angielski', 6, 3, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Język angielski', 7, 3, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Język angielski', 8, 3, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Język niemiecki', 8, 2, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Matematyka', 4, 4, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Matematyka', 5, 4, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Matematyka', 6, 4, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Matematyka', 7, 4, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Matematyka', 8, 4, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Informatyka', 4, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Informatyka', 5, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Informatyka', 6, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Informatyka', 7, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Informatyka', 8, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Technika', 4, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Technika', 5, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Technika', 6, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Historia', 4, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Historia', 5, 2, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Historia', 6, 2, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Historia', 7, 2, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Historia', 8, 2, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Przyroda', 4, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Muzyka', 4, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Muzyka', 5, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Muzyka', 6, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Muzyka', 7, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Plastyka', 4, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Plastyka', 5, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Plastyka', 6, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Plastyka', 7, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Wychowanie fizyczne', 4, 4, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Wychowanie fizyczne', 5, 4, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Wychowanie fizyczne', 6, 4, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Wychowanie fizyczne', 7, 4, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Wychowanie fizyczne', 8, 4, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Geografia', 5, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Biologia', 5, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Geografia', 6, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Biologia', 6, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Geografia', 7, 2, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Geografia', 7, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Biologia', 7, 2, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Biologia', 8, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Chemia', 7, 2, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Chemia', 8, 2, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Fizyka', 7, 2, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Fizyka', 8, 2, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Edukacja dla bezpieczeństwa', 7, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Edukacja dla bezpieczeństwa', 8, 1, DEFAULT);
+INSERT INTO Przedmiot VALUES (DEFAULT, 'Wiedza o spoeczeństwie', 8, 1, DEFAULT);
+
+
+
+
+
+
+
+
+INSERT INTO Nauczyciel_Przedmiot VALUES ('Njeden', 1);
+INSERT INTO Nauczyciel_Przedmiot VALUES ('Njeden', 2);
+
+COMMIT;
